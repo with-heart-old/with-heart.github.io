f5a912dcb4d9e72709a99d05c8a088c1678d6f74
with.heart <with.heart@protonmail.com>
2018-09-16 05:55:51 -0400

    feat: Remove post page and add styling to template

--- src/templates/blog-post.js
+++ src/templates/blog-post.js
@@ -1,16 +1,23 @@
 import React from 'react'
 import {graphql} from 'gatsby'
+import {css} from 'emotion'
+import tw from 'tailwind.macro'
 import Layout from '../components/Layout'
+
+const styles = {
+  container: css(tw`px-2 max-w-md mx-auto`),
+}
 
 export default ({data}) => {
   const post = data.markdownRemark
 
   return (
     <Layout>
-      <div>
+      <article className={styles.container}>
         <h1>{post.frontmatter.title}</h1>
+
         <div dangerouslySetInnerHTML={{__html: post.html}} />
-      </div>
+      </article>
     </Layout>
   )
 }
